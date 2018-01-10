1717c6d30c4126454cf936e9422ffcc9a014f390
Jeremy Morrell <jmorrell@heroku.com>
2018-01-10 13:18:47 -0800

    Add event loop metrics

---
+++ src/event-loop.js
@@ -0,0 +1,43 @@
+
+const EventEmitter = require('events');
+const INTERVAL = 10;
+const COUNTER_INTERVAL_DEFAULT = 4000; // 4 seconds
+
+function diffToMs(diff) {
+  return (diff[0] /*seconds*/ * 1000) + (diff[1] /*nanoseconds*/ / 1000000);
+}
+
+class EventLoopMonitor extends EventEmitter {
+  constructor() {
+    super();
+    this._loopMonitor = null;
+    this._counter = null;
+    this._time = process.hrtime();
+    this._ticks = [];
+  }
+
+  stop() {
+    clearInterval(this._loopMonitor);
+    clearInterval(this._counter);
+  }
+
+  start(counterInterval=COUNTER_INTERVAL_DEFAULT) {
+    this.stop();
+    this.measureTick(counterInterval);
+    this._counter = setInterval(() => {
+      this.emit('data', { ticks: this._ticks });
+      this._ticks = [];
+    }, counterInterval);
+  }
+
+  measureTick(counterInterval) {
+    const start = process.hrtime();
+    this._loopMonitor = setTimeout(() => {
+      const diff = process.hrtime(start);
+      this._ticks.push(Math.floor(diffToMs(diff) - INTERVAL));
+      this.measureTick();
+    }, INTERVAL);
+  }
+}
+
+module.exports = EventLoopMonitor;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const gc = (require('gc-stats'))();
 const request = require('request');
+const EventLoopMonitor = require('./event-loop');
+
+// Add timers that approximate the delay in the event loop
+const eventLoopMonitor = new EventLoopMonitor();
+eventLoopMonitor.start();
 
 // pauseNS is the cumulative GC pause of the node program between GC runs.
 // This is reset every metrics submission run.
@@ -17,6 +22,15 @@ let heapSizeLimit = 0;
 // gcCount is the number of garbage collections that have been observed between
 // metrics runs. This is reset every metrics submission run.
 let gcCount = 0;
+
+// latencies contains the counts for various "buckets" of event loop latencies
+// Ex: all latencies between 1ms and 2ms will be counted in the "1ms" bucket
+// This is reset every metrics submission run.
+let latencies = {};
+
+// This contains our estimations for the amount of time the event loop is
+// active. This is reset every metrics submission run.
+let eventLoopPercentageEstimates = []
 
 // metricsURL is where the runtime metrics will be posted to. This is added
 // to dynos by runtime iff the app is opped into the heroku runtime metrics
@@ -36,6 +50,50 @@ gc.on('stats', function (stats) {
     heapSizeLimit = stats.after.heapSizeLimit;
 });
 
+eventLoopMonitor.on('data', ({ticks}) => {
+  const total = ticks.reduce((acc, time) => acc + time, 0);
+  eventLoopPercentageEstimates.push(total / 4000);
+
+  // This takes an array of ms latencies and buckets them. The buckets are
+  // currently arbitrary, but can be decided based upon the needs of the
+  // visualization in the metrics dashboard.
+  ticks.reduce((acc, time) => {
+    if (time <= 0) {
+      acc['0'] = acc['0'] ? acc['0'] + 1 : 1;
+    } else if (time === 1) {
+      acc['1'] = acc['1'] ? acc['1'] + 1 : 1;
+    } else if (time === 2) {
+      acc['2'] = acc['2'] ? acc['2'] + 1 : 1;
+    } else if (time <= 5) {
+      acc['5'] = acc['5'] ? acc['5'] + 1 : 1;
+    } else if (time <= 10) {
+      acc['10'] = acc['10'] ? acc['10'] + 1 : 1;
+    } else if (time <= 20) {
+      acc['20'] = acc['20'] ? acc['20'] + 1 : 1;
+    } else if (time <= 50) {
+      acc['50'] = acc['50'] ? acc['50'] + 1 : 1;
+    } else if (time <= 100) {
+      acc['100'] = acc['100'] ? acc['100'] + 1 : 1;
+    } else if (time <= 200) {
+      acc['200'] = acc['200'] ? acc['200'] + 1 : 1;
+    } else if (time <= 400) {
+      acc['400'] = acc['400'] ? acc['400'] + 1 : 1;
+    } else if (time <= 1000) {
+      acc['1000'] = acc['1000'] ? acc['1000'] + 1 : 1;
+    } else if (time <= 2000) {
+      acc['2000'] = acc['2000'] ? acc['2000'] + 1 : 1;
+    } else if (time > 2000) {
+      acc['2000+'] = acc['1000'] ? acc['1000'] + 1 : 1;
+    }
+    return acc;
+  }, latencies);
+});
+
+function averageArray(arr) {
+  const sum = arr.reduce((a, b) => a + b, 0);
+  return sum / arr.length;
+}
+
 // every 20 seconds, submit a metrics payload to metricsURL.
 setInterval(function() {
     // the metrics data collected above.
@@ -47,7 +105,10 @@ setInterval(function() {
         gauges: {
             "node.heap.inuse.bytes": usedHeapSize,
             "node.heap.total.bytes": totalHeapSize,
-            "node.heap.limit.bytes": heapSizeLimit
+            "node.heap.limit.bytes": heapSizeLimit,
+            "node.eventloop.usage.percent": averageArray(
+              eventLoopPercentageEstimates
+            )
         }
     };
 
@@ -79,4 +140,6 @@ setInterval(function() {
 
     pauseNS = 0;
     gcCount = 0;
+    eventLoopPercentageEstimates = [];
+    latencies = {};
 }, 20000); // 20 seconds
