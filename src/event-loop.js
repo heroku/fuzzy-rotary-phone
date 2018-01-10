af775c1fdb326762750c2bfafc62f08f98ed0b53
Jeremy Morrell <jmorrell@heroku.com>
2018-01-10 13:25:32 -0800

    Style tweaks + run the code through prettier

--- src/event-loop.js
+++ src/event-loop.js
@@ -1,10 +1,9 @@
-
-const EventEmitter = require('events');
+const EventEmitter = require("events");
 const INTERVAL = 10;
 const COUNTER_INTERVAL_DEFAULT = 4000; // 4 seconds
 
 function diffToMs(diff) {
-  return (diff[0] /*seconds*/ * 1000) + (diff[1] /*nanoseconds*/ / 1000000);
+  return diff[0] /*seconds*/ * 1000 + diff[1] /*nanoseconds*/ / 1000000;
 }
 
 class EventLoopMonitor extends EventEmitter {
@@ -21,11 +20,11 @@ class EventLoopMonitor extends EventEmitter {
     clearInterval(this._counter);
   }
 
-  start(counterInterval=COUNTER_INTERVAL_DEFAULT) {
+  start(counterInterval = COUNTER_INTERVAL_DEFAULT) {
     this.stop();
     this.measureTick(counterInterval);
     this._counter = setInterval(() => {
-      this.emit('data', { ticks: this._ticks });
+      this.emit("data", { ticks: this._ticks });
       this._ticks = [];
     }, counterInterval);
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const gc = (require('gc-stats'))();
-const request = require('request');
-const EventLoopMonitor = require('./event-loop');
+const gc = require("gc-stats")();
+const request = require("request");
+const EventLoopMonitor = require("./event-loop");
 
 // Add timers that approximate the delay in the event loop
 const eventLoopMonitor = new EventLoopMonitor();
@@ -30,7 +30,7 @@ let latencies = {};
 
 // This contains our estimations for the amount of time the event loop is
 // active. This is reset every metrics submission run.
-let eventLoopPercentageEstimates = []
+let eventLoopPercentageEstimates = [];
 
 // metricsURL is where the runtime metrics will be posted to. This is added
 // to dynos by runtime iff the app is opped into the heroku runtime metrics
@@ -41,16 +41,16 @@ const metricsURL = process.env.HEROKU_METRICS_URL;
 const metricsInterval = 20000; // 20 seconds
 
 // on every garbage collection, update the statistics.
-gc.on('stats', function (stats) {
-    gcCount++;
+gc.on("stats", stats => {
+  gcCount++;
 
-    pauseNS = pauseNS + stats.pause;
-    usedHeapSize = stats.after.usedHeapSize;
-    totalHeapSize = stats.after.totalHeapSize;
-    heapSizeLimit = stats.after.heapSizeLimit;
+  pauseNS = pauseNS + stats.pause;
+  usedHeapSize = stats.after.usedHeapSize;
+  totalHeapSize = stats.after.totalHeapSize;
+  heapSizeLimit = stats.after.heapSizeLimit;
 });
 
-eventLoopMonitor.on('data', ({ticks}) => {
+eventLoopMonitor.on("data", ({ ticks }) => {
   const total = ticks.reduce((acc, time) => acc + time, 0);
   eventLoopPercentageEstimates.push(total / 4000);
 
@@ -59,31 +59,31 @@ eventLoopMonitor.on('data', ({ticks}) => {
   // visualization in the metrics dashboard.
   ticks.reduce((acc, time) => {
     if (time <= 0) {
-      acc['0'] = acc['0'] ? acc['0'] + 1 : 1;
+      acc["0"] = acc["0"] ? acc["0"] + 1 : 1;
     } else if (time === 1) {
-      acc['1'] = acc['1'] ? acc['1'] + 1 : 1;
+      acc["1"] = acc["1"] ? acc["1"] + 1 : 1;
     } else if (time === 2) {
-      acc['2'] = acc['2'] ? acc['2'] + 1 : 1;
+      acc["2"] = acc["2"] ? acc["2"] + 1 : 1;
     } else if (time <= 5) {
-      acc['5'] = acc['5'] ? acc['5'] + 1 : 1;
+      acc["5"] = acc["5"] ? acc["5"] + 1 : 1;
     } else if (time <= 10) {
-      acc['10'] = acc['10'] ? acc['10'] + 1 : 1;
+      acc["10"] = acc["10"] ? acc["10"] + 1 : 1;
     } else if (time <= 20) {
-      acc['20'] = acc['20'] ? acc['20'] + 1 : 1;
+      acc["20"] = acc["20"] ? acc["20"] + 1 : 1;
     } else if (time <= 50) {
-      acc['50'] = acc['50'] ? acc['50'] + 1 : 1;
+      acc["50"] = acc["50"] ? acc["50"] + 1 : 1;
     } else if (time <= 100) {
-      acc['100'] = acc['100'] ? acc['100'] + 1 : 1;
+      acc["100"] = acc["100"] ? acc["100"] + 1 : 1;
     } else if (time <= 200) {
-      acc['200'] = acc['200'] ? acc['200'] + 1 : 1;
+      acc["200"] = acc["200"] ? acc["200"] + 1 : 1;
     } else if (time <= 400) {
-      acc['400'] = acc['400'] ? acc['400'] + 1 : 1;
+      acc["400"] = acc["400"] ? acc["400"] + 1 : 1;
     } else if (time <= 1000) {
-      acc['1000'] = acc['1000'] ? acc['1000'] + 1 : 1;
+      acc["1000"] = acc["1000"] ? acc["1000"] + 1 : 1;
     } else if (time <= 2000) {
-      acc['2000'] = acc['2000'] ? acc['2000'] + 1 : 1;
+      acc["2000"] = acc["2000"] ? acc["2000"] + 1 : 1;
     } else if (time > 2000) {
-      acc['2000+'] = acc['1000'] ? acc['1000'] + 1 : 1;
+      acc["2000+"] = acc["1000"] ? acc["1000"] + 1 : 1;
     }
     return acc;
   }, latencies);
@@ -95,51 +95,52 @@ function averageArray(arr) {
 }
 
 // every 20 seconds, submit a metrics payload to metricsURL.
-setInterval(function() {
-    // the metrics data collected above.
-    data = {
-        counters: {
-            "node.gc.collections": gcCount,
-            "node.gc.pause.ns": pauseNS
-        },
-        gauges: {
-            "node.heap.inuse.bytes": usedHeapSize,
-            "node.heap.total.bytes": totalHeapSize,
-            "node.heap.limit.bytes": heapSizeLimit,
-            "node.eventloop.usage.percent": averageArray(
-              eventLoopPercentageEstimates
-            )
-        }
-    };
+setInterval(() => {
+  // the metrics data collected above.
+  data = {
+    counters: {
+      "node.gc.collections": gcCount,
+      "node.gc.pause.ns": pauseNS
+    },
+    gauges: {
+      "node.heap.inuse.bytes": usedHeapSize,
+      "node.heap.total.bytes": totalHeapSize,
+      "node.heap.limit.bytes": heapSizeLimit,
+      "node.eventloop.usage.percent": averageArray(eventLoopPercentageEstimates)
+    }
+  };
 
-    // post data to metricsURL
-    options = {
-        method: "POST",
-        uri: metricsURL,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data),
-        timeout: 100 // at max stops customer app for 100 ms
-    };
+  // post data to metricsURL
+  options = {
+    method: "POST",
+    uri: metricsURL,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
 
-    let cb = function(error, resp, body) {
-        if (error != null) {
-            console.log("[fuzzy-rotary-phone] error when trying to submit data: ", error);
-            return;
-        }
+  request(options, (error, resp, body) => {
+    if (error !== null) {
+      console.log(
+        "[fuzzy-rotary-phone] error when trying to submit data: ",
+        error
+      );
+      return;
+    }
 
-        if (resp.statusCode != 200) {
-            console.log("[fuzzy-rotary-phone] expected 200 when trying to submit data, got:", resp.statusCode);
-            console.log("[fuzzy-rotary-phone] body:", body);
-            return;
-        }
-    };
+    if (resp.statusCode !== 200) {
+      console.log(
+        "[fuzzy-rotary-phone] expected 200 when trying to submit data, got:",
+        resp.statusCode
+      );
+      console.log("[fuzzy-rotary-phone] body:", body);
+      return;
+    }
+  });
 
-    request(options, cb);
-
-    pauseNS = 0;
-    gcCount = 0;
-    eventLoopPercentageEstimates = [];
-    latencies = {};
+  pauseNS = 0;
+  gcCount = 0;
+  eventLoopPercentageEstimates = [];
+  latencies = {};
 }, 20000); // 20 seconds
